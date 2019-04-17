9669a1611f5090a1cd5cc4ac41dc179a4f6dd010
Upadhye <606184@cognizant.com>
2019-04-17 13:33:14 +0530

    initial commit

---
+++ src/main/java/com/practice/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,62 @@
+package com.practice.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+	
+	@Id
+	private Long id;
+	
+	@Column(name="currency_from")
+	private String from;
+	
+	@Column(name="currency_to")
+	private String to;
+	
+	private BigDecimal conversionFactor;
+	private int port;
+	
+	public ExchangeValue() {
+		
+	}
+	
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionFactor) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+
+}
